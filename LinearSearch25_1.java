64020b97b17dd45085af54cdb2d1573ac3bae015
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-10-24 15:26:44 +0700

    Modif terakhir

---
+++ LinearSearch25_1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class LinearSearch25_1 {
+    public static void main(String[] args) {
+        Scanner sc25 = new Scanner (System.in);
+
+        int [] nilaiKey = new int [10];
+        int jmlKey = 0;
+        int hasil = 0;
+        int cari = 0;
+        
+
+        System.out.print("Masukkan jumlah elemen array : ");
+        jmlKey = sc25.nextInt();
+
+        for (int i = 0; i < jmlKey; i++) {
+            System.out.print("Masukkan elemen array ke-"+(i+1)+" : ");
+            nilaiKey[i] = sc25.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+        cari = sc25.nextInt();
+        for (int i = 0; i < jmlKey; i++){
+            if (nilaiKey[i] == cari){
+                hasil = i;
+                System.out.println("Key ada di posisi ke- " +i);
+                break;
+            }else{
+                System.out.println("Key tidak dapat ditemukan");
+                break;
+            }
+        }
+    }
+}
